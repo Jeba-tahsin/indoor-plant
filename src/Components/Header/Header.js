fdbf08b09c3e1a1da8b8fda8e614597e52568f67
Jeba-tahsin <jebatahsin500@.com>
2021-09-26 22:55:00 +0600

    react

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 const Header = () => {
     return (
         <div className='header'>
-            <h1>Indor Plants: The Small-Space Garden</h1>
+            <h1>Indoor Plants: The Small-Space Garden</h1>
             <p>Nothing adds more beauty and comfort to our homes and offices than the lush flowers and foliage of indoor plants.Bringing plants into home is aesthetically pleasing and - amazingly - plants can offer strong health benefits as well! </p>
             <h3>Total plant species: 391,000 species</h3>
         </div>
--- src/Components/PlantDetails/PlantDetails.js
+++ src/Components/PlantDetails/PlantDetails.js
@@ -4,19 +4,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSeedling } from '@fortawesome/free-solid-svg-icons'
 
 const PlantDetails = (props) => {
-    console.log(props.plant)
+    
     const { name, price, carelevel, img, pros, lightLevel} = props.plant;
     const cartHandler = props.cartHandler;
     return (
-    <div class="col">
-        <div class="card h-100 plantinfo">
-        <img src={img} class="card-img-top" alt="..."/>
-        <div class="card-body">
-            <h4 class="card-title">{name}</h4>
+    <div className="col">
+        <div className="card h-100 plantinfo">
+        <img src={img} className="card-img-top" alt="..."/>
+        <div className="card-body">
+            <h4 className="card-title">{name}</h4>
             <h6>Care level: {carelevel}</h6>
             <h6>Light level: {lightLevel}</h6>
             <h6>Price: ${price}</h6>
-            <p class="card-text">Pros: {pros}</p>
+            <p className="card-text">Pros: {pros}</p>
             <button onClick={()=>cartHandler(props.plant)} className='btn button'><FontAwesomeIcon icon={faSeedling} />Click</button>
         </div>
         </div>
